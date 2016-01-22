217d6cf58b2f721f21beb70800453ad225766e6f
ABAmvrosov <abamvrosov@gmail.com>
2016-01-22 16:36:31 +0300

    week3

--- SuperSpartyBros/Assets/Scripts/EnemyStun.cs
+++ SuperSpartyBros/Assets/Scripts/EnemyStun.cs
@@ -10,6 +10,9 @@ public class EnemyStun : MonoBehaviour {
 		{
 			// tell the enemy to be stunned
 			this.GetComponentInParent<Enemy>().Stunned();
+
+			// make the player bounce off the enemy (aka, junp)
+			other.gameObject.GetComponent<CharacterController2D>().EnemyBounce();
 		}
 	}
 }
